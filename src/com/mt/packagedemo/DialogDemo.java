d50a300fa80c839d77ec4bc3cef235b6b67a0dbe
KrishnaYamphu <krishnayamphu@gmail.com>
2022-06-26 18:54:30 +0545

    package

---
+++ src/com/mt/packagedemo/DialogDemo.java
@@ -0,0 +1,9 @@
+package com.mt.packagedemo;
+
+import javax.swing.JOptionPane;
+
+public class DialogDemo {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hello world","Test",JOptionPane.INFORMATION_MESSAGE);
+    }
+}
